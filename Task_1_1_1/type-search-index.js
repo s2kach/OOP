4ece28fb521d5de81af50bb1963979536ab480ba
s2kach <s2kach@users.noreply.github.com>
2025-09-12 06:39:50 +0000

    Deploying to gh-pages from @ s2kach/OOP@6d88ec2468ae2613bc81554f8f804662a051a21d 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.dizmestev","l":"Sort"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.dizmestev","l":"Sort"},{"p":"ru.nsu.dizmestev","l":"Sort_checktime"}];updateSearchResults();
